bd1e86dc080539a19e3bf9e7c5d877e3d3963167
John Charman <john.charman@imhotek.net>
2022-09-14 11:25:14 +0100

    tests added

--- src/Validator/Utility/Indifferent.php
+++ src/Validator/Utility/Indifferent.php
@@ -2,7 +2,13 @@
 
 namespace Membrane\Validator\Utility;
 
-class Indifferent
-{
+use Membrane\Result\Result;
+use Membrane\Validator;
 
+class Indifferent implements Validator
+{
+    public function validate(mixed $value): Result
+    {
+        return Result::noResult($value);
+    }
 }
