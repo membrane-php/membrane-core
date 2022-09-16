fc0b12894427c6d95784eec4c684823e0ff7418d
John Charman <john.charman@imhotek.net>
2022-09-16 14:53:58 +0100

    oneOf validator implemented

--- src/Validator/Logical/OneOf.php
+++ src/Validator/Logical/OneOf.php
@@ -19,47 +19,54 @@ class OneOf implements Validator
 
     public function validate(mixed $value): Result
     {
-        $result = Result::noResult($value);
+        $resultChain = [];
+        $messageSetChain = [];
 
         foreach ($this->chain as $item) {
-            $result = $this->fullMerge($result, $item->validate($value));
-            if ($result->result === Result::VALID) {
-                return $result;
+            $itemResult = $item->validate($value);
+
+            switch ($itemResult->result) {
+                case Result::VALID:
+                    return $itemResult;
+                case Result::INVALID:
+                    $messageSetChain [] = $itemResult->messageSets[0];
+                case Result::NO_RESULT:
+                    $resultChain [] = $itemResult->result;
             }
         }
 
-        return $result;
-    }
+        $result = $this->mergeResult($resultChain);
 
-    public function fullMerge(Result $currentResult, Result $newResult): Result
-    {
-        $result = $this->mergeResult($currentResult, $newResult);
-
-        $mergedMessageSet = new MessageSet(null);
-
-        if ($result !== Result::VALID) {
-            foreach ($currentResult->messageSets as $messageSet) {
-                $mergedMessageSet = $mergedMessageSet->merge($messageSet);
-            }
-
-            foreach ($newResult->messageSets as $messageSet) {
-                $mergedMessageSet = $mergedMessageSet->merge($messageSet);
-            }
+        $messageSet = new MessageSet(null);
+        if ($result === Result::INVALID) {
+            $messageSet = $this->mergeMessages($messageSetChain);
         }
 
         return new Result(
-            $newResult->value,
+            $value,
             $result,
-            ...($mergedMessageSet->isEmpty() ? [] : [$mergedMessageSet])
+            ...($messageSet->isEmpty() ? [] : [$messageSet])
         );
     }
 
-    private function mergeResult(Result $currentResult, Result $newResult): int
+    private function mergeResult(array $results): int
     {
-        if ($newResult->result === Result::NO_RESULT && $currentResult->result === Result::NO_RESULT) {
-            return Result::NO_RESULT;
+        foreach ($results as $result) {
+            if ($result === Result::INVALID) {
+                return $result;
+            }
+        }
+        return Result::NO_RESULT;
+    }
+
+    private function mergeMessages(array $messageSets): MessageSet
+    {
+        $mergedMessageSet = new MessageSet(null);
+
+        foreach ($messageSets as $messageSet) {
+            $mergedMessageSet = $mergedMessageSet->merge($messageSet);
         }
 
-        return $newResult->result;
+        return $mergedMessageSet;
     }
 }
--- tests/Validator/Logical/OneOfTest.php
+++ tests/Validator/Logical/OneOfTest.php
@@ -9,6 +9,7 @@ use Membrane\Result\MessageSet;
 use Membrane\Result\Result;
 use Membrane\Validator\Logical\OneOf;
 use Membrane\Validator\Utility\Fails;
+use Membrane\Validator\Utility\Indifferent;
 use Membrane\Validator\Utility\Passes;
 use PHPUnit\Framework\TestCase;
 
@@ -22,13 +23,23 @@ use PHPUnit\Framework\TestCase;
  */
 class OneOfTest extends TestCase
 {
+    public function DataSetsThatReturnNoResult(): array
+    {
+        return [
+            [[]],
+            [[new Indifferent()]],
+            [[new Indifferent(), new Indifferent(), new Indifferent()]]
+        ];
+    }
+
     /**
      * @test
+     * @dataProvider DataSetsThatReturnNoResult
      */
-    public function NoValidatorReturnsNoResult(): void
+    public function NoResultsReturnsNoResult(array $chain): void
     {
         $input = 'this can be anything';
-        $oneOf = new OneOf();
+        $oneOf = new OneOf(...$chain);
         $expected = Result::noResult($input);
 
         $result = $oneOf->validate($input);
@@ -36,85 +47,50 @@ class OneOfTest extends TestCase
         self::assertEquals($expected, $result);
     }
 
-    /**
-     * @test
-     */
-    public function SinglePassReturnsValid(): void
+    public function DataSetsThatReturnInvalid(): array
     {
-        $input = 'this can be anything';
-        $oneOf = new OneOf(new Passes);
-        $expected = Result::valid($input);
-
-        $result = $oneOf->validate($input);
-
-        self::assertEquals($expected, $result);
-    }
-
-    /**
-     * @test
-     */
-    public function MultiplePassesReturnsValid(): void
-    {
-        $input = 'this can be anything';
-        $oneOf = new OneOf(new Passes, new Passes, new Passes);
-        $expected = Result::valid($input);
-
-        $result = $oneOf->validate($input);
-
-        self::assertEquals($expected, $result);
-    }
-
-    /**
-     * @test
-     */
-    public function SingleFailsReturnsInvalid(): void
-    {
-        $input = 'this can be anything';
-        $oneOf = new OneOf(new Fails);
         $expectedMessage = new Message('I always fail', []);
-        $expected = Result::invalid($input, new MessageSet(null, $expectedMessage));
+        return [
+            [[new Fails()], new MessageSet(null, $expectedMessage)],
+            [[new Fails(), new Fails(), new Fails()], new MessageSet(null, $expectedMessage, $expectedMessage, $expectedMessage)],
+            [[new Indifferent(), new Fails(), new Indifferent(), new Indifferent()], new MessageSet(null, $expectedMessage)]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider DataSetsThatReturnInvalid
+     */
+    public function SingleFailsReturnsInvalid(array $chain, MessageSet $expectedMessageSet): void
+    {
+        $input = 'this can be anything';
+        $oneOf = new OneOf(...$chain);
+        $expected = Result::invalid($input, $expectedMessageSet);
 
         $result = $oneOf->validate($input);
 
         self::assertEquals($expected, $result);
     }
 
-    /**
-     * @test
-     */
-    public function MultipleFailsReturnsInvalid(): void
+    public function DataSetsThatReturnValid(): array
     {
-        $input = 'this can be anything';
-        $oneOf = new OneOf(new Fails, new Fails, new Fails);
-        $expectedMessage = new Message('I always fail', []);
-        $expected = Result::invalid($input, new MessageSet(null, $expectedMessage, $expectedMessage, $expectedMessage));
-
-        $result = $oneOf->validate($input);
-
-        self::assertEquals($expected, $result);
+        return [
+            [[new Passes()]],
+            [[new Indifferent(), new Passes()]],
+            [[new Fails(), new Passes()]],
+            [[new Fails(), new Indifferent(), new Passes()]],
+            [[new Fails(), new Indifferent(), new Passes(), new Fails(), new Fails(), new Indifferent()]]
+        ];
     }
 
     /**
      * @test
+     * @dataProvider DataSetsThatReturnValid
      */
-    public function PassAndFailsReturnsValid(): void
+    public function AnyValidResultsReturnsValid(array $chain): void
     {
         $input = 'this can be anything';
-        $oneOf = new OneOf(new Fails, new Passes);
-        $expected = Result::valid($input);
-
-        $result = $oneOf->validate($input);
-
-        self::assertEquals($expected, $result);
-    }
-
-    /**
-     * @test
-     */
-    public function MultiplePassAndFailsReturnsValid(): void
-    {
-        $input = 'this can be anything';
-        $oneOf = new OneOf(new Fails, new Passes, new Fails, new Passes);
+        $oneOf = new OneOf(...$chain);
         $expected = Result::valid($input);
 
         $result = $oneOf->validate($input);
