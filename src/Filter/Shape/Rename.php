bdffd91d5d1da776aa5bf3ba064cc11da5623378
John Charman <john.charman@imhotek.net>
2022-09-20 17:22:07 +0100

    Rename filter implemented

--- src/Filter/Shape/Rename.php
+++ src/Filter/Shape/Rename.php
@@ -5,12 +5,36 @@ declare(strict_types=1);
 namespace Membrane\Filter\Shape;
 
 use Membrane\Filter;
+use Membrane\Result\Message;
+use Membrane\Result\MessageSet;
 use Membrane\Result\Result;
 
 class Rename implements Filter
 {
+    public function __construct(
+        private readonly mixed $old,
+        private readonly mixed $new
+    )
+    {
+    }
+
     public function filter(mixed $value): Result
     {
-        // TODO: Implement filter() method.
+        if (!is_array($value)) {
+            $message = new Message('Rename filter requires arrays, %s given', [gettype($value)]);
+            return Result::invalid($value, new MessageSet(null, $message));
+        }
+
+        if (array_is_list($value)) {
+            $message = new Message('Rename filter requires arrays with key-value pairs', []);
+            return Result::invalid($value, new MessageSet(null, $message));
+        }
+
+        if (isset($value[$this->old])) {
+            $value[$this->new] = $value[$this->old];
+            unset($value[$this->old]);
+        }
+
+        return Result::noResult($value);
     }
 }
--- tests/Filter/Shape/RenameTest.php
+++ tests/Filter/Shape/RenameTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Filter\Shape;
 
 use Membrane\Filter\Shape\Rename;
+use Membrane\Result\Message;
+use Membrane\Result\MessageSet;
+use Membrane\Result\Result;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -12,5 +15,82 @@ use PHPUnit\Framework\TestCase;
  */
 class RenameTest extends TestCase
 {
+    public function DataSetsWithIncorrectInputs(): array
+    {
+        $notArrayMessage = 'Rename filter requires arrays, %s given';
+        $listMessage = 'Rename filter requires arrays with key-value pairs';
+        return [
+            [123, new Message($notArrayMessage, ['integer'])],
+            [1.23, new Message($notArrayMessage, ['double'])],
+            ['this is a string', new Message($notArrayMessage, ['string'])],
+            [true, new Message($notArrayMessage, ['boolean'])],
+            [['this', 'is', 'a', 'list'], new Message($listMessage, [])],
+        ];
+    }
 
+    /**
+     * @test
+     * @dataProvider DataSetsWithIncorrectInputs
+     */
+    public function IncorrectInputsReturnInvalid(mixed $input, Message $expectedMessage): void
+    {
+        $expected = Result::invalid($input, new MessageSet(null, $expectedMessage));
+        $rename = new Rename('old key', 'new key');
+
+        $result = $rename->filter($input);
+
+        self::assertEquals($expected, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function NonExistentKeysAreIgnored(): void
+    {
+        $input = [1 => 'a', 2 => 'b'];
+        $expected = Result::noResult($input);
+        $rename = new Rename(3, 4);
+
+        $result = $rename->filter($input);
+
+        self::assertEquals($expected, $result);
+    }
+
+    public function DataSetsToFilter(): array
+    {
+        return [
+            [
+                ['this' => 'is', 'an' => 'array'],
+                'this',
+                'that',
+                ['that' => 'is', 'an' => 'array']
+            ],
+            [
+                [1 => 'a', 2 => 'b'],
+                1,
+                'one',
+                ['one' => 'a', 2 => 'b']
+            ],
+            [
+                ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4],
+                'b',
+                2,
+                ['a' => 1, 2.4 => 2, 'c' => 3, 'd' => 4]
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider DataSetsToFilter
+     */
+    public function IfOldKeyExistsThenItIsRenamed(array $input, mixed $old, mixed $new, array $expectedValue): void
+    {
+        $expected = Result::noResult($expectedValue);
+        $rename = new Rename($old, $new);
+
+        $result = $rename->filter($input);
+
+        self::assertEquals($expected, $result);
+    }
 }
