59a3e4bbab84ef48abe4ec214c4ffa2bd205f216
John Charman <john.charman@imhotek.net>
2022-10-03 17:10:57 +0100

    fixtures namespace added

---
+++ tests/fixtures/create-password.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Membrane\Fixtures;
+
+use Membrane\Attribute\Builder;
+
+include_once __DIR__ . '/../../vendor/autoload.php';
+
+$builder = new Builder();
