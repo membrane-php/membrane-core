a13f2dfa6231427777bb1d2271cd54990b06a4ca
John Charman <john.charman@imhotek.net>
2022-11-24 19:10:32 +0000

    Validator: IsNumber added

---
+++ tests/Validator/Type/IsNumberTest.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Validator\Type;
+
+use Membrane\Result\Message;
+use Membrane\Result\MessageSet;
+use Membrane\Result\Result;
+use Membrane\Validator\Type\IsNumber;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Membrane\Validator\Type\IsNumber
+ * @uses   \Membrane\Result\Message
+ * @uses   \Membrane\Result\MessageSet
+ * @uses   \Membrane\Result\Result
+ */
+class IsNumberTest extends TestCase
+{
+
+    public function dataSetsToValidate(): array
+    {
+        $class = new class {
+        };
+
+        return [
+            'validates integer' => [
+                1,
+                Result::valid(1),
+            ],
+            'validates float' => [
+                2.3,
+                Result::valid(2.3),
+            ],
+            'invalidates integer string' => [
+                '4',
+                Result::invalid(
+                    '4',
+                    new MessageSet(null, new Message('Value must be int or float, %s passed', ['string']))
+                ),
+            ],
+            'invalidates float string' => [
+                '5.6',
+                Result::invalid(
+                    '5.6',
+                    new MessageSet(null, new Message('Value must be int or float, %s passed', ['string']))
+                ),
+            ],
+            'invalidates non-numeric string' => [
+                'six',
+                Result::invalid(
+                    'six',
+                    new MessageSet(null, new Message('Value must be int or float, %s passed', ['string']))
+                ),
+            ],
+            'invalidates bool' => [
+                true,
+                Result::invalid(
+                    true,
+                    new MessageSet(null, new Message('Value must be int or float, %s passed', ['boolean']))
+                ),
+            ],
+            'invalidates null' => [
+                null,
+                Result::invalid(
+                    null,
+                    new MessageSet(null, new Message('Value must be int or float, %s passed', ['NULL']))
+                ),
+            ],
+            'invalidates array' => [
+                [1, 2, 3],
+                Result::invalid([1, 2, 3],
+                    new MessageSet(null, new Message('Value must be int or float, %s passed', ['array']))),
+            ],
+            'invalidates object' => [
+                $class,
+                Result::invalid(
+                    $class,
+                    new MessageSet(null, new Message('Value must be int or float, %s passed', ['object']))
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider dataSetsToValidate
+     */
+    public function validateTest(mixed $value, Result $expected): void
+    {
+        $sut = new IsNumber();
+
+        $actual = $sut->validate($value);
+
+        self::assertEquals($expected, $actual);
+    }
+
+}
