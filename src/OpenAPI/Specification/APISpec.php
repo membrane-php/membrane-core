0e3eab9b142769e94888cbdabd7dc3452cfec11d
Carnage <t.carnage@gmail.com>
2022-12-31 22:28:45 +0000

    Fixing CS issues

--- src/OpenAPI/Specification/APISpec.php
+++ src/OpenAPI/Specification/APISpec.php
@@ -44,7 +44,7 @@ abstract class APISpec implements Specification
             }
         }
 
-            $this->matchingPath ?? throw CannotProcessRequest::pathNotFound(
+        $this->matchingPath ?? throw CannotProcessRequest::pathNotFound(
             pathinfo($filePath, PATHINFO_BASENAME),
             $url
         );
@@ -84,7 +84,7 @@ abstract class APISpec implements Specification
             } elseif ($fileExtension === 'yml' || $fileExtension === 'yaml') {
                 return Reader::readFromYamlFile($filePath);
             }
-        } catch (TypeError|TypeErrorException|ParseException $e) {
+        } catch (TypeError | TypeErrorException | ParseException $e) {
             throw CannotReadOpenAPI::cannotParse(pathinfo($filePath, PATHINFO_BASENAME), $e);
         } catch (UnresolvableReferenceException $e) {
             throw CannotReadOpenAPI::unresolvedReference(pathinfo($filePath, PATHINFO_BASENAME), $e);
