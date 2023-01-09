33e13afe60cda362c295fe31f00c689fd9de319f
John Charman <john.charman@imhotek.net>
2023-01-09 14:27:06 +0000

    DefaultField Processor added

---
+++ src/Processor/DefaultField.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Membrane\Processor;
+
+use Membrane\Filter;
+use Membrane\Processor;
+use Membrane\Result\FieldName;
+use Membrane\Result\Result;
+use Membrane\Validator;
+
+class DefaultField implements Processor
+{
+    private readonly Field $field;
+
+    public function __construct(Filter|Validator ...$chain)
+    {
+        $this->field = new Field('', ...$chain);
+    }
+
+    public function processes(): string
+    {
+        return $this->field->processes();
+    }
+
+    public function process(FieldName $parentFieldName, mixed $value): Result
+    {
+        return $this->field->process($parentFieldName, $value);
+    }
+}
