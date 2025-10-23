8f9260a8cf32ff642cd857e3673078e8eb9deb45
John Charman <john.charman@imhotek.net>
2025-10-23 12:40:21 +0100

    Add typehint to __toString() method

--- src/Filter.php
+++ src/Filter.php
@@ -10,7 +10,7 @@ interface Filter
 {
     public function filter(mixed $value): Result;
 
-    public function __toString();
+    public function __toString(): string;
 
     public function __toPHP(): string;
 }
