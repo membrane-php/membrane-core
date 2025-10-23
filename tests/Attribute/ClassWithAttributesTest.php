09d29b9ecffea105adc035458de0b3f0796f707c
John Charman <john.charman@imhotek.net>
2025-10-23 12:40:43 +0100

    Replace coverage annotation with coverage attribute

--- tests/Attribute/ClassWithAttributesTest.php
+++ tests/Attribute/ClassWithAttributesTest.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace Membrane\Tests\Attribute;
 
 use Membrane\Attribute\ClassWithAttributes;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @covers \Membrane\Attribute\ClassWithAttributes
- */
+#[CoversClass(\Membrane\Attribute\ClassWithAttributes::class)]
 class ClassWithAttributesTest extends TestCase
 {
     #[Test]
